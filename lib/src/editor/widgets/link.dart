1b76b3b73b864183c3c260759a2b17a62787a86e
mny <mny9@outlookc.om>
2025-05-30 18:32:37 +0800

    adapter ohos322

--- lib/src/editor/widgets/link.dart
+++ lib/src/editor/widgets/link.dart
@@ -216,7 +216,7 @@ class _CupertinoAction extends StatelessWidget {
             Icon(
               icon,
               size: theme.iconTheme.size,
-              color: theme.colorScheme.onSurface.withValues(alpha: 0.75),
+              color: theme.colorScheme.onSurface.withOpacity(0.75),
             )
           ],
         ),
